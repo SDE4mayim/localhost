aae22c5b588f6d0598e8d20293e7e60ef01a818b
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-08-08 12:50:00 +0530

    Delete cypress directory

--- cypress/adding pet/localhost.cy.js
+++
@@ -1,7 +0,0 @@
-describe('open',() =>{
-    it('verify',() =>{
-       cy.visit(' http://localhost:5173/')
-       cy.get('.grow > :nth-child(2) > .font-medium').click()
-       cy.get('#username').type("visrutha")
-})
-})
