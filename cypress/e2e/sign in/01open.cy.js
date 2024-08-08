d626dcdac5ad6e1290fa1664e92c6ad58da1452d
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-08-08 13:03:58 +0530

    Delete cypress/e2e directory

--- cypress/e2e/sign in/01open.cy.js
+++
@@ -1,7 +0,0 @@
-describe('open',() =>{
-    it('verify',() =>{
-        cy.visit(' http://localhost:5173/')
-        cy.title().should("eq","VET CASTLE")
-    })
-}
-)
--- cypress/e2e/sign up/01signup.cy.js
+++
@@ -1,9 +0,0 @@
-describe('open',() =>{
-    it('verify',() =>{
-        cy.visit(' http://localhost:5173/')
-        cy.get(".btn-sm").click()
-        cy.title().should("eq","VET CASTLE")
-         cy.url().should("eq","http://localhost:5173/signup")
-    })
-}
-)
