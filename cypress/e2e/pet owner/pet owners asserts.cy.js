fac9bbc35c05655d00129e6ca4f35e1ee6880d33
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-08-10 12:53:50 +0530

    pet owners asserts.cy.js

--- cypress/e2e/pet owner/pet owners asserts.cy.js
+++ cypress/e2e/pet owner/pet owners asserts.cy.js
@@ -8,9 +8,8 @@ describe('first',()=>{
    cy.get('.btn').click()
    cy.wait(1000)
    cy.reload()
-  cy.visit("http://localhost:5173/maindashboard")
-    
-          cy.url().should('eq','http://localhost:5173/maindashboard')
+   cy.visit("http://localhost:5173/maindashboard")
+   cy.url().should('eq','http://localhost:5173/maindashboard')
    cy.get('[href="/csd"] > .MuiListItemText-root > .MuiTypography-root').click()
    cy.url().should('eq','http://localhost:5173/csd')
    cy.get('[data-id="400001"] > [data-field="action"] > .cellAction > .viewbutton').click()
