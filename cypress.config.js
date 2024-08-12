e42b8a529cb92a137bb1937a4180436f48483a15
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-08-12 16:53:06 +0530

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    chromeWebSecurity: false, // Add this line to disable Chrome web security
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
 
-{
-  "chromeWebSecurity": false
-}
